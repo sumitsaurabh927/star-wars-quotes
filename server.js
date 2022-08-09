60ab43f01b179be3ffb396bf0cf7a2df1fe8d305
Sumit Saurabh <62152915+sumitsaurabh927@users.noreply.github.com>
2022-08-09 10:28:22 +0530

    html file response by server

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+console.log('may node be with you!');
+
+app.listen(3000, () => console.log(`listening on port 3000`));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+
+
