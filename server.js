c1b31e9f7860630000083c186eac2238c547ca3e
Sumit Saurabh <62152915+sumitsaurabh927@users.noreply.github.com>
2022-08-09 12:01:32 +0530

    post request to DB done

--- server.js
+++ server.js
@@ -30,15 +30,46 @@ let connectionString = 'mongodb+srv://starwars:8Jzi35na2qa9myZe@cluster0.jwijhwf
 
 // promise way of connecting to DB and catching error
 MongoClient.connect(connectionString)
-    .then(random => {
+    .then(client => {
         console.log('connected to DB');
-    })
-    .catch(err => console.error(err));
+
+        // connect to the actual DB and not the default one
+        const db = client.db('star-war-quotes');
+
+        // sample methods
+        // app.use(/* ... */)
+        // app.get(/* ... */)
+        // app.post(/* ... */)
+        // app.listen(/* ... */)
+
+        // create a new collection
+        // remember DB is a room; collection is like boxes inside that room
+
+        const allQuotes = db.collection('quotes');
+
+
+
+        app.post('/quotes', (req, res) => {
+            // console.log(`Response received on '/quotes'`)
+            // console.log('.');
+            // console.log(req.body)
+        
+            // sending stuff to the DB
+            allQuotes.insertOne(req.body)
+                .then(result => {
+                    console.log(result);
+                    res.redirect('/')
+                }).catch(err => console.error(err));
+        })
+
+
+
+    }).catch(err => console.error(err));
 
 // *************************************************************
 // CRUD handlers
 // listen on PORT for http requests
-app.listen(3000, () => console.log(`listening on port 3000`));
+app.listen(3001, () => console.log(`listening on port 3000`));
 
 // GET request handling
 app.get('/', (req, res) => {
@@ -46,8 +77,14 @@ app.get('/', (req, res) => {
 })
 
 // POST request handling
-app.post('/quotes', (req, rest) => {
-    console.log(`Response received on '/quotes'`)
-    console.log('.');
-    console.log(req.body)
-})
+// app.post('/quotes', (req, res) => {
+//     // console.log(`Response received on '/quotes'`)
+//     // console.log('.');
+//     // console.log(req.body)
+
+//     // sending stuff to the DB
+//     allQuotes.insertOne(req.body)
+//         .then(result => {
+//             console.log(result);
+//         }).catch(err => console.error(err));
+// })
